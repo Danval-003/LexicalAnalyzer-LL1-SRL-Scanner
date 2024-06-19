eb6d3c605ad03a129dd843a022eae7f4be2c4bdb
Daniel Armando Valdez Reyes <77825594+Danval-003@users.noreply.github.com>
2024-06-19 17:50:46 -0600

    Prepare Angular to deploy

--- frontend/src/app/pages/simulater/simulater.component.ts
+++ frontend/src/app/pages/simulater/simulater.component.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SimulateText } from '../../../apiCalls';
@@ -5,33 +6,32 @@ import { SimulateText } from '../../../apiCalls';
 @Component({
   selector: 'app-simulater',
   standalone: true,
-  imports: [
-    CommonModule
-  ],
+  imports: [CommonModule],
   templateUrl: './simulater.component.html',
-  styleUrls: ['./simulater.component.scss']
+  styleUrls: ['./simulater.component.scss'],
 })
 export class SimulaterComponent {
-
   success: boolean = false;
   message: string = '';
   resultScanner: string = '';
 
-  constructor() { }
+  constructor() {}
 
   onSimulate(content: string, scanner: string, slr: string) {
     console.log(content, scanner, slr);
-    SimulateText(content, scanner, slr).then((response) => {
-      console.log(response);
-      this.success = response.sim.accept;
-      this.message = response.message;
-      this.resultScanner = response.scannerResult;
-      console.log(this.success);
-    }).catch((error) => {
-      console.log(error);
-      this.success = false;
-      this.message = error.message; // Mostrar el mensaje de error específico
-      this.resultScanner = '';
-    });
+    SimulateText(content, scanner, slr)
+      .then((response: any) => {
+        console.log(response);
+        this.success = response.sim?.accept || false;
+        this.message = response.message || 'No message returned';
+        this.resultScanner = response.scannerResult || 'No scanner result';
+        console.log(this.success);
+      })
+      .catch((error: any) => {
+        console.log(error);
+        this.success = false;
+        this.message = error.message || 'An error occurred';
+        this.resultScanner = '';
+      });
   }
 }
